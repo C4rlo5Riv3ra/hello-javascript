9a9fc0fd9261c5dc90d360f468a19ed8c63d48ad
c4rlo5Riv3ra <jcriveraguadalupe@gmail.com>
2024-08-01 22:36:05 -0500

    Solucion de Primera Tarea Begginer

--- Basic/03-beginner-exercises.js
+++ Basic/03-beginner-exercises.js
@@ -6,20 +6,65 @@ https://www.twitch.tv/videos/2200149072?t=00h08m02s
 
 // 1. Escribe un comentario en una línea
 
+//mi primer comentario
+
 // 2. Escribe un comentario en varias líneas
+
+/* comentario de
+varias lineas */
 
 // 3. Declara variables con valores asociados a todos los datos de tipo primitivos
 
+let name = "CarlosDev"
+let age = 22
+let isStudent = true
+let height = 1.77
+
 // 4. Imprime por consola el valor de todas las variables
+
+console.log(name)
+console.log(age)
+console.log(isStudent)
+console.log(height)
 
 // 5. Imprime por consola el tipo de todas las variables
 
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof isStudent)
+console.log(typeof height)
+
 // 6. A continuación, modifica los valores de las variables por otros del mismo tipo
+
+name = "RiveraDev"
+age = 23
+isStudent = false
+height = 1.75
 
 // 7. A continuación, modifica los valores de las variables por otros de distinto tipo
 
+name = 25
+age = "CarlosDev"
+isStudent = 1.77
+height = true
+
 // 8. Declara constantes con valores asociados a todos los tipos de datos primitivos
+
+const nameDev = "CarlosDev"
+const ageDev = 22
+const isStudentDev = true
+const heightDev = 1.77
 
 // 9. A continuación, modifica los valores de las constantes
 
+nameDev = "RiveraDev"
+ageDev = 23
+
 // 10. Comenta las líneas que produzcan algún tipo de error al ejecutarser
+
+/* Error: la variable const no se puede reasignar
+nameDev = "RiveraDev"
+console.log(nameDev)
+ageDev = 23
+console.log(ageDev)
+*/
